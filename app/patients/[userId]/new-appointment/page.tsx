950aa44b352fd18aef916ab293eedfde79f4d872
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-07-22 21:40:24 +0700

    Add appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,40 @@
-const NewAppointment = () => {
-    return (
-        <div>NewAppointment</div>
-    )
-}
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
 
-export default NewAppointment
+export default async function NewAppointment({ params: { userId } }: SearchParamProps) {
+    const patient = await getPatient(userId)
+    return (
+        <div className="flex h-screen max-h-screen">
+            <section className="remove-scrollbar container my-auto">
+                <div className="sub-container max-w-[860px] flex-1 justify-between">
+                    <Image
+                        src="/assets/icons/logo-full.svg"
+                        width={1000}
+                        height={1000}
+                        alt="Patient"
+                        className="mb-12 h-10 w-fit"
+                    />
+
+                    <AppointmentForm
+                        type="create"
+                        userId={userId}
+                        patientId={patient.$id}
+                    />
+
+                    <p className="copyright mt-10 py-12">
+                        © 2024 HealthCare
+                    </p>
+                </div>
+            </section>
+
+            <Image
+                src="/assets/images/appointment-img.png"
+                width={1000}
+                height={1000}
+                alt="Appointment"
+                className="side-img max-w-[390px] bg-bottom"
+            />
+        </div>
+    );
+}
